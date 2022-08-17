e7ab47967f834fb08085d399030afc77f13de674
taemin4u <taemin4u@khu.ac.kr>
2022-08-18 05:07:57 +0900

    'navCSS'

--- React/Master/twitter/src/components/Navigation.js
+++ React/Master/twitter/src/components/Navigation.js
@@ -1,18 +1,45 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import '../basic.css';
+
+const Ul = styled.ul`
+  list-style: none;
+  text-align: center;
+  width: 300px;
+  margin: 0 auto;
+  color: white;
+  margin-bottom: 20px;
+  margin-top: 30px;
+`;
+
+const Li = styled.li`
+  margin-bottom: 5px;
+  border: 1px solid black;
+  background-color: #3b1e1e;
+  border-radius: 5px;
+
+  &:hover {
+    transition: transform 0.2s ease-in;
+    transform: scale(1.2);
+  }
+`;
 
 function Navigation({ userObj }) {
-  console.log('nav', userObj);
   return (
     <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="profile">{userObj.email}'s Profile</Link>
-        </li>
-      </ul>
+      <Ul>
+        <Li>
+          <Link to="/" className="link">
+            Home
+          </Link>
+        </Li>
+        <Li>
+          <Link to="profile" className="link">
+            {userObj.email}'s Profile
+          </Link>
+        </Li>
+      </Ul>
     </nav>
   );
 }
